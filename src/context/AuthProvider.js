ae86bb84a5d9b22c3fdaea05b9ccff224f881f51
MacBook <macbook@MacBooks-MacBook-Air.local>
2022-11-08 03:05:19 +0600

    add login

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,47 @@
+import React, { createContext, useEffect, useState } from 'react';
+import{getAuth, onAuthStateChanged, signInWithEmailAndPassword}from 'firebase/auth'
+import app from '../firebase/firebase.config';
+
+
+export const AuthContext=createContext()
+const auth=getAuth(app)
+
+const AuthProvider = ({children}) => {
+    const [user,setUser]=useState(null)
+    const [loading, setLoading] = useState(true);
+
+    //login 
+    const login = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    //
+
+
+
+
+
+    // create user
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log(currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () =>{
+            return unsubscribe();
+        }
+    }, [])
+
+
+    const Info={login,user,loading}
+    return (
+        <AuthContext.Provider value={Info}>
+            {children}
+
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/pages/FromPages/Loginpage/Login.js
+++ src/pages/FromPages/Loginpage/Login.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider';
 
 const Login = () => {
+    const {login}=useContext(AuthContext)
     const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
-        const password = form.password.value;}
+        const password = form.password.value;
+        login(email,password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err => console.error(err));
+
+    
+    
+    
+    
+    
+    
+    
+    
+    }
 
     return (
         <div className="hero w-full my-20">
